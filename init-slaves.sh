ebfbb94ce3e62d7e2c2afbdfe7619d6b5b1960a5
Muhammet İpek <40898553+ipekmuhammet@users.noreply.github.com>
2020-07-09 04:40:17 +0300

    Update init-slaves.sh

--- init-slaves.sh
+++ init-slaves.sh
@@ -10,4 +10,5 @@ rs.reconfig(cfg); \
 rs.add({ host: \"mongo2:27017\", priority: 0.5 }); \
 rs.status(); \
 "
-docker exec -it $(docker ps -q)  bash -c "echo '${replicate}' | mongo"
+
+docker exec -it $(docker ps -f name=mongo -q)  bash -c "echo '${replicate}' | mongo"
